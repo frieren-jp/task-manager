82dcc1885eddaa924e40852f75aca68c92a773e3
frieren-jp <hornet1311@gmail.com>
2026-03-20 15:15:05 +0300

    added A-Z sorter

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ export default function App() {
   const [tasks, setTasks] = useLocalStorage("tasks", []);
   const [search, setSearch] = useState("");
   const [theme, setTheme] = useState("light");
+  const [sortOrder, setSortOrder] = useState("asc");
 
-  // 🔥 ВАЖНО: применяем тему ко всему body
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
@@ -24,17 +24,36 @@ export default function App() {
   const updateTask = (id, text) =>
     setTasks(tasks.map(t => t.id === id ? { ...t, text } : t));
 
-  const filtered = tasks.filter(t =>
-    t.text.toLowerCase().includes(search.toLowerCase())
-  );
+
+  const filtered = tasks
+    .filter(t =>
+      t.text.toLowerCase().includes(search.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.text.localeCompare(b.text);
+      } else {
+        return b.text.localeCompare(a.text);
+      }
+    });
 
   return (
     <div style={{ padding: 20 }}>
       <h1>Task Manager</h1>
 
       <ThemeToggle theme={theme} setTheme={setTheme} />
+
       <SearchBar setSearch={setSearch} />
+
+      {}
+      <button onClick={() =>
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc")
+      }>
+        Sort: {sortOrder === "asc" ? "A-Z" : "Z-A"}
+      </button>
+
       <TaskInput addTask={addTask} />
+
       <TaskList
         tasks={filtered}
         deleteTask={deleteTask}
